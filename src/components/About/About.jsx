04bc937635c7fae46421c391883cb681c9c48fd3
Edward Cobbold <edward.cobbold@gmail.com>
2017-04-19 09:53:46 +0100

    editorconfig: 4 spaces for SCSS only

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,19 +8,19 @@ import PageHeader    from '../PageHeader/PageHeader'
 // import AboutSection  from '../AboutSection/AboutSection'
 
 class About extends React.Component {
-    constructor(props) {
-        super(props)
-    } // /constructor(props)
+  constructor(props) {
+    super(props)
+  } // /constructor(props)
 
-    render() {
-        return (
-            <PageContainer>
-                <PageContent>
-                    <PageHeader title="Hello"/>
-                </PageContent>
-            </PageContainer>
-        )
-    } // /render()
+  render() {
+    return (
+      <PageContainer>
+        <PageContent>
+          <PageHeader title="Hello"/>
+        </PageContent>
+      </PageContainer>
+    )
+  } // /render()
 } // /class About extends Component
 
 export default About
