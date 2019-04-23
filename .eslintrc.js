646d82fc18e49f82b0f8ef118ab674cc54da39b4
Edward Cobbold <edward.cobbold@gmail.com>
2019-04-23 23:56:28 +0100

    About: use function + hooks, add eslint hooks rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,30 @@
 module.exports = {
-  "plugins": [
-    "react",
-    "react-hooks",
-    "jest",
-    "jsx-a11y"
-  ],
-  "extends": [
-    "react-app",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "airbnb"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    }
+  plugins: ['react', 'react-hooks', 'jest', 'jsx-a11y'],
+  extends: ['react-app', 'plugin:react/recommended', 'plugin:jsx-a11y/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  "globals": {
-    "jest": true
+  globals: {
+    jest: true,
   },
-  "env": {
-    "jest/globals": true
+  env: {
+    'jest/globals': true,
   },
-  "settings": {
-    "import/resolver": {
-      "webpack": {
-        "config": "webpack.config.dev.js"
-      }
-    }
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'webpack.config.dev.js',
+      },
+    },
   },
-  "rules": {
-    "consistent-return": "off",
-    "react/jsx-one-expression-per-line": "off",
-    "react/no-unescaped-entities": ["off", "'"],
-    "dot-notation": "off"
-  }
-}
+  rules: {
+    'consistent-return': 'off',
+    'dot-notation': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/no-unescaped-entities': ['off', "'"],
+  },
+};
--- client/src/components/About/About.component.jsx
+++ client/src/components/About/About.component.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
@@ -16,111 +16,105 @@ import AboutIcon from 'components/AboutIcon/AboutIcon.component';
 
 import classes from './About.component.scss';
 
-class About extends PureComponent {
-  componentDidMount() {
-    const { aboutSections, hasFetched, aboutSectionsRequest } = this.props;
-
-    if (!hasFetched && !aboutSections.length) {
+const About = ({ aboutSections, hasFetched, aboutSectionsRequest }) => {
+  useEffect(() => {
+    if (!hasFetched && aboutSections.length === 0) {
       aboutSectionsRequest();
     }
-  }
+  }, [hasFetched, aboutSections, aboutSectionsRequest]);
 
-  render() {
-    const { aboutSections } = this.props;
+  return (
+    <Page>
+      <PageHeader title="Hello" scrollLabel="Scroll for about" isFullscreen>
+        <PageHeaderSubtitle>
+          I'm a Frontend developer from Kent, working in London.
+        </PageHeaderSubtitle>
 
-    return (
-      <Page>
-        <PageHeader title="Hello" scrollLabel="Scroll for about" isFullscreen>
-          <PageHeaderSubtitle>
-            I'm a Frontend developer from Kent, working in London.
-          </PageHeaderSubtitle>
+        <PageHeaderSubtitle>
+          <hr className={classes['about__page-header__divider']} />
+          <Link to="/portfolio">Check out my portfolio</Link>
+        </PageHeaderSubtitle>
+      </PageHeader>
 
-          <PageHeaderSubtitle>
-            <hr className={classes['about__page-header__divider']} />
-            <Link to="/portfolio">Check out my portfolio</Link>
-          </PageHeaderSubtitle>
-        </PageHeader>
-
-        <PageContent>
-          <section className={classes['about__page-content']}>
-            <Container>
-              <Row justify="center">
-                <Column spanSM={10} spanLG={12} spanXL={10}>
-                  {aboutSections.map(section => (
-                    <AboutSection
-                      key={`about-section--${section.id}`}
-                      title={section.title}
-                    >
-                      {/* If 1 icon, house content in columns */}
-                      {!!section.icons.length && section.icons.length === 1
-                        ? (
-                          <Row justify="center">
-                            <Column spanMD={8}>
-                              <Row alignMD="middle">
-                                {!!section.icons.length && (
-                                  <Column span={12} spanMD={4} pushMD={8}>
-                                    <figure
-                                      className={classnames(
-                                        classes['about__section-graphic'],
-                                        classes[`about__section-graphic--${section.slug}`],
-                                        classes['about__section-graphic--single'],
-                                      )}
-                                    >
-                                      {section.icons.map(icon => (
-                                        <AboutIcon
-                                          key={`${section.key}__icon--${icon.name}`}
-                                          iconName={icon.name}
-                                          large={icon.large}
-                                        />
-                                      ))}
-                                    </figure>
-                                  </Column>
-                                )}
-                                <Column span={12} spanMD={8} pullMD={4}>
-                                  <ReactMarkdown source={section.content} />
+      <PageContent>
+        <section className={classes['about__page-content']}>
+          <Container>
+            <Row justify="center">
+              <Column spanSM={10} spanLG={12} spanXL={10}>
+                {aboutSections.map(section => (
+                  <AboutSection
+                    key={`about-section--${section.id}`}
+                    title={section.title}
+                  >
+                    {/* If 1 icon, house content in columns */}
+                    {section.icons.length === 1
+                      ? (
+                        <Row justify="center">
+                          <Column spanMD={8}>
+                            <Row alignMD="middle">
+                              {!!section.icons.length && (
+                                <Column span={12} spanMD={4} pushMD={8}>
+                                  <figure
+                                    className={classnames(
+                                      classes['about__section-graphic'],
+                                      classes[`about__section-graphic--${section.slug}`],
+                                      classes['about__section-graphic--single'],
+                                    )}
+                                  >
+                                    {section.icons.map(icon => (
+                                      <AboutIcon
+                                        key={`${section.key}__icon--${icon.name}`}
+                                        iconName={icon.name}
+                                        large={icon.large}
+                                      />
+                                    ))}
+                                  </figure>
                                 </Column>
-                              </Row>
-                            </Column>
-                          </Row>
-                        )
-                        : (
-                          <Fragment>
-                            {!!section.icons.length && (
-                              <figure
-                                className={classnames(
-                                  classes['about__section-graphic'],
-                                  classes[`about__section-graphic--${section.slug}`],
-                                )}
-                              >
-                                {section.icons.map(icon => (
-                                  <AboutIcon
-                                    key={`${section.key}__icon--${icon.name}`}
-                                    iconName={icon.name}
-                                    large={icon.large}
-                                  />
-                                ))}
-                              </figure>
-                            )}
-
-                            <Row justify="center">
-                              <Column spanMD={8}>
+                              )}
+                              <Column span={12} spanMD={8} pullMD={4}>
                                 <ReactMarkdown source={section.content} />
                               </Column>
                             </Row>
-                          </Fragment>
-                        )
-                      }
-                    </AboutSection>
-                  ))}
-                </Column>
-              </Row>
-            </Container>
-          </section>
-        </PageContent>
-      </Page>
-    );
-  }
-}
+                          </Column>
+                        </Row>
+                      )
+                      : (
+                        <Fragment>
+                          {section.icons.length > 0 && (
+                            <figure
+                              className={classnames(
+                                classes['about__section-graphic'],
+                                classes[`about__section-graphic--${section.slug}`],
+                              )}
+                            >
+                              {section.icons.map(icon => (
+                                <AboutIcon
+                                  key={`${section.key}__icon--${icon.name}`}
+                                  iconName={icon.name}
+                                  large={icon.large}
+                                />
+                              ))}
+                            </figure>
+                          )}
+
+                          <Row justify="center">
+                            <Column spanMD={8}>
+                              <ReactMarkdown source={section.content} />
+                            </Column>
+                          </Row>
+                        </Fragment>
+                      )
+                    }
+                  </AboutSection>
+                ))}
+              </Column>
+            </Row>
+          </Container>
+        </section>
+      </PageContent>
+    </Page>
+  );
+};
 
 About.propTypes = {
   aboutSections: PropTypes.arrayOf(
