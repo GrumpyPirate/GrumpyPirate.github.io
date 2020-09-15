6df5daa7e0407621deb139dac0125d020df03be0
Edward Cobbold <edward.cobbold@gmail.com>
2020-09-15 20:01:13 +0100

    build(gh-pages): gh-pages setup

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, HashRouter as Router, Switch } from 'react-router-dom';
 import CSSTransition from 'react-transition-group/CSSTransition';
 import TransitionGroup from 'react-transition-group/TransitionGroup';
 
