c595b20ca9fd7a6eab385a578e8eed9c683adeb0
Edward Cobbold <edward.cobbold@gmail.com>
2020-09-21 17:36:33 +0100

    feat(icons): add: gatsby, graphql, jest, parcel, sketch

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -3,33 +3,33 @@ import styled from 'styled-components';
 
 import { IconProps } from './Icon.types';
 
-// Brand icons
 import 'images/icons/airplay.svg';
 import 'images/icons/angular.svg';
 import 'images/icons/babel.svg';
 import 'images/icons/bootstrap.svg';
 import 'images/icons/bulma.svg';
-// feather-icons
 import 'images/icons/chevron-down.svg';
 import 'images/icons/chevron-left.svg';
 import 'images/icons/contentful.svg';
 import 'images/icons/divio.svg';
 import 'images/icons/django.svg';
 import 'images/icons/foundation.svg';
+import 'images/icons/gatsby.svg';
+import 'images/icons/graphql.svg';
 import 'images/icons/grunt.svg';
 import 'images/icons/gulp.svg';
 import 'images/icons/hash.svg';
-// Generic
 import 'images/icons/heart.svg';
 import 'images/icons/html5.svg';
 import 'images/icons/illustrator.svg';
 import 'images/icons/image.svg';
 import 'images/icons/invision.svg';
 import 'images/icons/javascript.svg';
+import 'images/icons/jest.svg';
 import 'images/icons/laravel.svg';
-// Utils
 import 'images/icons/loader.svg';
 import 'images/icons/nextjs.svg';
+import 'images/icons/parcel.svg';
 import 'images/icons/photoshop.svg';
 import 'images/icons/postcss.svg';
 import 'images/icons/react.svg';
